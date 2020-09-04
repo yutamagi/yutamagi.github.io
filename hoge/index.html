8d8589cda13b19d256d10570e6210f20ccccdd76
yuta3617 <yuta3617@gmail.com>
2020-09-05 08:08:45 +0900

    test

--- hoge/index.html
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-  <title>github pages test</title>
-</head>
-<body>
-  <h1>github pages test</h1>
-</body>
-</html>
---
+++ src/components/Persons/Person/Person.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './Person.module.css';
+
+const Person = (props) => {
+  console.log('[Person.js] rendering...');
+    return (
+        <div className={classes.Person}>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
+        </div>
+    );
+}
+
+export default Person;
